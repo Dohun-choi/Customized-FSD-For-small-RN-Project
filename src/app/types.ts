d7bf083aa7db31493c21777955867c9aea7a9df5
최도훈 <chlehgns123@naver.com>
2024-03-23 19:22:56 +0900

    fix: 타입 관련 쓸모없는 코드 제거

--- src/app/types.ts
+++ src/app/types.ts
@@ -5,21 +5,18 @@ import { RouteProp as A } from "@react-navigation/native";
 
 type NativeStackParamList = RootParamList & HomeParamList;
 
-type RootScreenType = {
-	[key in keyof NativeStackParamList]: NativeStackParamList[key];
-};
-type Route<T extends keyof RootScreenType> = A<RootScreenType, T>;
+type Route<T extends keyof NativeStackParamList> = A<NativeStackParamList, T>;
 
 declare global {
-	type Navigation = NativeStackNavigationProp<RootScreenType>;
-	interface RootRouteProp<T extends keyof RootScreenType> {
+	type Navigation = NativeStackNavigationProp<NativeStackParamList>;
+	interface RootRouteProp<T extends keyof NativeStackParamList> {
 		route: Route<T>;
 	}
-	interface RootNavigaionProp {
-		navigation: NativeStackNavigationProp<RootScreenType>;
+	interface RootNavigationProp {
+		navigation: NativeStackNavigationProp<NativeStackParamList>;
 	}
-	interface RootScreenProp<T extends keyof RootScreenType> {
-		navigation: NativeStackNavigationProp<RootScreenType>;
+	interface RootScreenProp<T extends keyof NativeStackParamList> {
+		navigation: NativeStackNavigationProp<NativeStackParamList>;
 		route: Route<T>;
 	}
 }
